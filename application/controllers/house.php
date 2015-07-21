2d26a3b52a7ae19426efa1bc9f49fa4e4177eb48
bin.shen <bluelazysb@hotmail.com>
2015-07-21 10:01:35 +0800

    1111

---
+++ application/controllers/house.php
@@ -0,0 +1,22 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+/**
+ * 房源列表页/详情页控制器
+ *
+ * @package		app
+ * @subpackage	core
+ * @category	controller
+ * @author		bin.shen
+ *
+ */
+class House extends MY_Controller {
+
+	public function __construct() {
+		parent::__construct();
+
+	}
+	
+	public function index() {
+		
+	}
+}
