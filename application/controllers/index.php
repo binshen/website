2deb323ea041794a183b0690a89065790d0f6e54
yaobin <bluelazysb@hotmail.com>
2015-07-22 15:27:44 +0800

    111

--- application/controllers/index.php
+++ application/controllers/index.php
@@ -17,7 +17,13 @@ class Index extends MY_Controller {
 	}
 	
 	public function index() {
+		$data = $this->sysconfig_model->get_index_info();
 		
+		$this->assign('news1', $data['news1']);
+		$this->assign('news2', $data['news2']);
+		$this->assign('news3', $data['news3']);
+		$this->assign('news4', $data['news4']);
+		$this->assign('news5', $data['news5']);
 		$this->display('index.html');
 	}
 }
--- application/models/sysconfig_model.php
+++ application/models/sysconfig_model.php
@@ -35,87 +35,15 @@ class Sysconfig_model extends MY_Model
         parent::__destruct();
     }
     
-	/**
-     * 获取页面标题关键字等属性
-     */
-	public function get_pageset()
-	{
-		$pageset=array(
-							'title'=>'捷安特单车运动服务(昆山)有限公司',
-							'keywords'=>'捷安特单车运动服务(昆山)有限公司',
-							'descrip'=>'捷安特单车运动服务(昆山)有限公司',
-							'ico'=>'images/favicon.ico',
-							'gif'=>'images/1.gif'
-							//'websit_0_1'=>'http://tmyegg.kssina.com'
-		);
-		return $pageset;
-	}
-	
-//	/**
-//     * 获取所有产品类别
-//     * @param $where 查询条件，为空则查询所有
-//     */
-//	public function get_product_type($where=null){
-//        $this->db->select('*');
-//        $this->db->from("{$this->tables[0]}");
-//        if($where){
-//        	$this->db->where('parent_id',$where);
-//        }
-//        $res = $this->db->get()->result_array();
-//		return $res;        
-//	}
-	
-//	/**
-//     * 获取一级目录下的二级子目录
-//     */
-//	public function get_memu(){
-//
-//		$this->db->select('id,title');
-//		$this->db->from("{$this->tables[1]}");
-//		$res['about'] = $this->db->get()->result_array();
-//		
-//		$this->db->select('id,name');
-//		$this->db->from("{$this->tables[2]}");
-//		$this->db->where('parent_id',0);
-//        $res['news'] = $this->db->get()->result_array();
-//        
-// 		$this->db->select('id,name');
-//		$this->db->from("{$this->tables[6]}");
-//        $res['team_type'] = $this->db->get()->result_array();
-//        
-//		return $res;        
-//	}
-	
-//	/**
-//     * 获取联系方式
-//     */
-//	public function get_contact(){
-//		$this->db->select('*')->from($this->tables[3]);
-//		return $this->db->get()->row_array();
-//	}
-	
-//	/**
-//     * 获取首页新闻信息
-//     */
-//	public function get_index_news(){
-//		$this->db->select('*')->from($this->tables[4])->order_by('order_num','desc')->limit('5');
-//		return $this->db->get()->result_array();
-//	}
-	
-//	/**
-//     * 获取首页团队信息
-//     */
-//	public function get_index_teams(){
-//		$this->db->select('id,name,img')->from($this->tables[5])->order_by('order_num')->limit('8');
-//		return $this->db->get()->result_array();
-//	}
-
-	public function insert($arr,$short){
-		foreach($arr as $k=>$v){
-			$this->db->insert('xiaoqu',array('name'=>$v,'short'=>$short));
-		}
-	}
-	
+    public function get_index_info(){
+    	$data['news1'] = $this->db->select('id,title,pic,title2')->from('news')->where('index_area','1')->order_by('cdate','desc')->limit(5,0)->get()->result_array();
+    	$data['news2'] = $this->db->select('id,title,pic,title2')->from('news')->where('index_area','2')->order_by('cdate','desc')->limit(12,0)->get()->result_array();
+    	$data['news3'] = $this->db->select('id,title,pic,title2')->from('news')->where('index_area','3')->order_by('cdate','desc')->get()->row_array();
+    	$data['news4'] = $this->db->select('id,title,pic,title2')->from('news')->where('index_area','4')->order_by('cdate','desc')->get()->row_array();
+    	$data['news5'] = $this->db->select('id,title,pic,title2')->from('news')->where('index_area','5')->order_by('cdate','desc')->limit(2,0)->get()->result_array();
+    	return $data;
+    }
+    
 }
 
 /* End of file sysconfig_model.php */
