7df9cd069b39faa6de8d84410c518587af085a84
bin.shen <bluelazysb@hotmail.com>
2015-07-28 13:54:20 +0800

    123123123

--- application/views/manage/index.php
+++ application/views/manage/index.php
@@ -2,7 +2,7 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<meta name="renderer" content="webkit">
+<meta name="renderer" content="webkit" />
 <title>sdbb</title>
 
 <link href="<?php echo base_url();?>dwz/themes/default/style.css" rel="stylesheet" type="text/css" media="screen"/>
--- application/views/manage/login.php
+++ application/views/manage/login.php
@@ -2,6 +2,7 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="renderer" content="webkit" />
 <title><?php echo $this->config->item('app_name');?></title>
 <link href="<?php echo base_url();?>dwz/themes/css/login.css" rel="stylesheet" type="text/css" />
 
