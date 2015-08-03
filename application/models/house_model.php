5bac1f6f1358e0c0cbeb2290f9f65795052eb7e2
yaobin <bluelazysb@hotmail.com>
2015-08-03 11:01:02 +0800

    11111111

--- application/models/house_model.php
+++ application/models/house_model.php
@@ -591,7 +591,12 @@ class House_model extends MY_Model
    		return $data;
    	}
    	
-   	public function get_article_detail($h_id,$id){
+   	public function get_article_detail($h_id,$id,$flag){
+   		if($flag){
+   			$rs = $this->db->select('id')->from('house')->where('xq_id',$h_id)->get()->row();
+   			$h_id = $rs->id;
+   		}
+   		
    		$data['tag'] = $this->db->select('a.id,xq_id,b.name region_name,a.name,region_id')->from('house a')->join('house_region b','a.region_id=b.id','left')->where('a.id',$h_id)->get()->row_array();
    		$data['detail'] = $this->db->select()->from('news')->where('id',$id)->get()->row_array();
    		return $data;
