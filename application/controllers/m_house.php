6619f9d5feace51a51e4cc035db7177426c74c81
bin.shen <bluelazysb@hotmail.com>
2015-09-22 15:32:57 +0800

    12312

--- application/controllers/m_house.php
+++ application/controllers/m_house.php
@@ -4,13 +4,21 @@ class M_house extends MY_Controller {
 
 	public function __construct() {
 		parent::__construct();
+		$this->load->model('house_model');
+		$this->load->model('manage_model');
 	}
 	
 	public function index() {
-		
 	}
 	
 	public function view_list() {
+		
+		$region_list = $this->house_model->get_m_house_region();
+		$this->assign('region_list', $region_list);
+		
+		$style_list = $this->house_model->get_search_style_list();
+		$this->assign('style_list', $style_list);
+		
 		$this->display('mobile/list.html');
 	}
 	
