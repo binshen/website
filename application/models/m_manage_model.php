5ff621792ef5a7b19765a265957a851fa44e7287
yaobin1340 <645894453@qq.com>
2015-10-09 09:09:24 +0800

    111

---
+++ application/models/m_manage_model.php
@@ -0,0 +1,74 @@
+<?php
+if (! defined('BASEPATH'))
+    exit('No direct script access allowed');
+
+/**
+ * 网站后台模型
+ *
+ * @package		app
+ * @subpackage	core
+ * @category	model
+ * @author		bin.shen
+ *        
+ */
+class M_manage_model extends MY_Model
+{
+    public function __construct ()
+    {
+        parent::__construct();
+    }
+
+    public function __destruct ()
+    {
+        parent::__destruct();
+    }
+
+    public function get_list($uid) {
+    	$this->db->select('a.*, b.name AS region_name, c.name AS orientation_name, d.name AS xq_name, d.address AS address, e.tel AS tel, f.name AS style_name');
+    	$this->db->from('house a');
+    	$this->db->join('house_region b', 'a.region_id = b.id', 'left');
+    	$this->db->join('house_orientation c', 'a.orientation_id = c.id', 'left');
+    	$this->db->join('xiaoqu d', 'a.xq_id = d.id', 'left');
+    	$this->db->join('admin e', 'a.broker_id = e.id', 'left');
+    	$this->db->join('house_substyle f', 'a.substyle_id = f.id', 'left');
+    	$this->db->where('a.type_id', 2);
+    	$this->db->where('user_id',$uid);
+    	
+    	$this->db->order_by('exe_status', 'desc');
+    	$this->db->order_by('refresh_time', 'desc');
+    	$this->db->order_by('a.id', 'desc');
+    	
+    	$data['res_list'] = $this->db->get()->result();
+    	
+    	$rs = $this->db->select('count(1) num')->from('house_collect')->where('user_id',$uid)->get()->row();
+    	$data['collect_count'] = $rs->num;
+    	
+    	return $data;
+    }
+    
+    public function refresh($id){
+    	$this->db->where('id',$id);
+    	return $this->db->update('house',array('refresh_time'=>date('Y-m-d H:i:s',time())));
+    }
+    
+    public function down_up($id,$exe_status){
+    	$this->db->where('id',$id);
+    	return $this->db->update('house',array('exe_status'=>$exe_status));
+    }
+    
+    public function get_house_detail($id){
+    	return $this->db->select()->from('house')->where('id',$id)->get()->row_array();
+    }
+    
+    public function post_edit(){
+    	$data = array(
+    			'name'=>$this->input->post('name'),
+    			'total_price'=>$this->input->post('total_price'),
+    			'acreage'=>$this->input->post('acreage'),
+    	);
+    	$this->db->where('id',$this->input->post('id'));
+    	return $this->db->update('house',$data);
+    }
+
+  
+}
