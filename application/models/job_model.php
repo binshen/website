cfdc7caa18a5f2f5283922a23d72f6969519b415
bin.shen <bluelazysb@hotmail.com>
2015-11-30 01:21:39 +0800

    123123

--- application/models/job_model.php
+++ application/models/job_model.php
@@ -14,16 +14,14 @@ class Job_model extends MY_Model
         parent::__destruct();
     }
     
-    function mostRepeatedValues($array,$length=0){
+    function mostRepeatedValues($array){
     	if(empty($array) or !is_array($array)){
     		return false;
     	}
     	$array = array_count_values($array);
     	arsort($array);
-    	if($length>0){
-    		$array = array_slice($array, 0, $length, true);
-    	}
-    	return $array;
+    	$array = array_slice($array, 0, 1, true);
+    	return array_values($array)[0];
     }
     
     public function match_house() {
@@ -36,41 +34,80 @@ class Job_model extends MY_Model
     		$this->db->join('house b', 'a.house_id = b.id', 'left');
     		
     		$house_ids = array();
-    		$search_region = array();
-    		$search_style = array();
-    		$search_price = array();
-    		$search_acreage = array();
-    		$search_type = array();
-    		$search_feature = array();
+    		$search_regions = array();
+    		$search_styles = array();
+    		$search_prices = array();
+    		$search_acreages = array();
+    		$search_types = array();
+    		$search_features = array();
     		$house_tracks = $this->db->where('a.open_id', $weixin['openid'])->get()->result_array();
-    		var_dump($house_tracks);
     		foreach ($house_tracks as $t) {
     			if(!empty($t['id'])) {
     				$house_ids[] =  $t['id'];
     			}
     			if(!empty($t['region_id'])) {
-    				$search_region[] = $t['region_id'];
+    				$search_regions[] = $t['region_id'];
     			}
     			if(!empty($t['substyle_id'])) {
-    				$search_style[] = $t['substyle_id'];
+    				$search_styles[] = $t['substyle_id'];
     			}
     			if(!empty($t['total_price'])) {
-    				$search_price[] = $t['total_price'];
+    				$total_price = floatval($t['total_price']);
+    				if($total_price <= 50) {
+    					$search_prices[] = 1;
+    				} else if($total_price <= 80) {
+    					$search_prices[] = 2;
+    				} else if($total_price <= 100) {
+    					$search_prices[] = 3;
+    				} else if($total_price <= 120) {
+    					$search_prices[] = 4;
+    				} else if($total_price <= 150) {
+    					$search_prices[] = 5;
+    				} else if($total_price <= 200) {
+    					$search_prices[] = 6;
+    				} else if($total_price <= 250) {
+    					$search_prices[] = 7;
+    				} else if($total_price <= 300) {
+    					$search_prices[] = 8;
+    				} else if($total_price <= 500) {
+    					$search_prices[] = 9;
+    				} else {
+    					$search_prices[] = 10;
+    				} 
     			}
     			if(!empty($t['acreage'])) {
-    				$search_acreage[] = $t['acreage'];
+    				$acreage = floatval($t['acreage']);
+    				if($acreage <= 50) {
+    					$search_acreages[] = 1;
+    				} else if($acreage <= 70) {
+    					$search_acreages[] = 2;
+    				} else if($acreage <= 90) {
+    					$search_acreages[] = 3;
+    				} else if($acreage <= 120) {
+    					$search_acreages[] = 4;
+    				} else if($acreage <= 150) {
+    					$search_acreages[] = 5;
+    				} else if($acreage <= 200) {
+    					$search_acreages[] = 6;
+    				} else if($acreage <= 300) {
+    					$search_acreages[] = 7;
+    				} else {
+    					$search_prices[] = 8;
+    				}
     			}
     			if(!empty($t['room'])) {
-    				$search_type[] = $t['room'];
+    				$search_types[] = $t['room'];
     			}
     			if(!empty($t['feature'])) {
-    				$search_feature[] = $t['feature'];
+    				$search_features[] = $t['feature'];
     			}
     		}
-    		var_dump($search_region);
-    		var_dump($this->mostRepeatedValues($search_region, 5));
+    		$search_region = $this->mostRepeatedValues($search_regions);
+    		$search_style = $this->mostRepeatedValues($search_styles);
+    		$search_price = $this->mostRepeatedValues($search_prices);
+    		$search_acreage = $this->mostRepeatedValues($search_acreages);
+    		$search_type = $this->mostRepeatedValues($search_types);
+    		$search_feature = $this->mostRepeatedValues($search_features);
     	}
-    	
-    	echo "============================";
     }
 }
