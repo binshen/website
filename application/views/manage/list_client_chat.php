ad583ded6d800143fe3e63bdd90a2594faf66965
bin.shen <bluelazysb@hotmail.com>
2015-12-21 13:18:17 +0800

    123123

--- application/views/manage/list_client_chat.php
+++ application/views/manage/list_client_chat.php
@@ -55,7 +55,7 @@
        </div>
   </div>
 <script src="/chat/js/single-iScroll.js" charset="gbk"></script>
-<script src="/chat/js/jquery-ui.min.js" charset="gbk"></script>
+<script type="text/javascript" src="/js/jquery-1.8.3.min.js" ></script>
 <script type="text/javascript" src="http://121.40.97.183:4000/socket.io/socket.io.js"></script>
 <script>
 $(function(){
