a6edbaf771f5cc2eabbc7d8a83cc522b6e8c2600
bin.shen <bluelazysb@hotmail.com>
2015-12-25 01:36:08 +0800

    12123

--- application/controllers/b_house.php
+++ application/controllers/b_house.php
@@ -281,7 +281,12 @@ class B_house extends MY_Controller {
 			$redis->connect('127.0.0.1', 6379);
 			if(!empty($o_bid)) {
 				$o_key = "map:" . $o_bid;
-				$redis->lrem($o_key, $open_id, 0);
+				//$redis->lrem($o_key, $open_id, 0);
+				$o_users = $redis->lrange($key, 0, -1);
+				if(in_array($open_id, $o_users)) {
+					unset($o_users[$o_key]);
+					$redis->lpush($o_key, $open_id);
+				}
 			}
 			
 			$key = "map:" . $broker_id;
