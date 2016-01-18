2e86df96279798d71c13363797d645f617cf5aa6
bin.shen <bluelazysb@hotmail.com>
2016-01-18 15:43:37 +0800

    123123

--- application/views/manage/add_broker.php
+++ application/views/manage/add_broker.php
@@ -117,4 +117,24 @@ function click_manager(i,j) {
 		$("#manager_" + j).prop("checked", false);
 	}
 }
+
+$("#fileField").change(function(){
+	var objUrl = getObjectURL(this.files[0]);
+	if (objUrl) {
+		html = '<img height="100px" src="'+objUrl+'" />';
+		$("#img").html(html) ;
+	}
+}) ;
+//建立一個可存取到該file的url
+function getObjectURL(file) {
+	var url = null ; 
+	if (window.createObjectURL!=undefined) { // basic
+		url = window.createObjectURL(file) ;
+	} else if (window.URL!=undefined) { // mozilla(firefox)
+		url = window.URL.createObjectURL(file) ;
+	} else if (window.webkitURL!=undefined) { // webkit or chrome
+		url = window.webkitURL.createObjectURL(file) ;
+	}
+	return url ;
+}
 </script>
