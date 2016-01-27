4b8507bcf46f3eeb4f2bf0139cebe51b5a044c89
bin.shen <bluelazysb@hotmail.com>
2016-01-27 10:26:06 +0800

    123123

---
+++ application/controllers/m_api.php
@@ -0,0 +1,20 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class M_api extends MY_Controller {
+
+	public function __construct() {
+		parent::__construct();
+		
+		$this->load->model('m_api_model');
+		
+		header('Content-Type: application/json');
+	}
+	
+	public function login() {
+		
+		$username = $this->input->post('username'); 
+		$password = $this->input->post('password');
+		$result = $this->m_api_model->login($username, $password);
+		echo json_encode($result);
+	}
+}
---
+++ application/models/m_api_model.php
@@ -0,0 +1,26 @@
+<?php
+if (! defined('BASEPATH'))
+    exit('No direct script access allowed');
+
+class M_api_model extends MY_Model {
+
+	public function __construct () {
+        parent::__construct();
+    }
+
+    public function __destruct () {
+        parent::__destruct();
+    }
+    
+    public function login($username, $password) {
+    	$this->db->from('admin');
+    	$this->db->where('username', $username);
+    	$this->db->where('passwd', sha1($password));
+    	$rs = $this->db->get();
+    	if ($rs->num_rows() > 0) {
+    		return json_encode(array('result' => true));
+    	} else {
+    		return json_encode(array('result' => false));
+    	}
+    }
+}
